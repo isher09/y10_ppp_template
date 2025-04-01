06b0c15a5a728b2969f193a43ccb7fb661703cc2
isherj <ijohal28@connect.kellettschool.com>
2025-04-01 05:21:38 +0000

    Apr 1, 2025, 5:21 AM

--- main.py
+++ main.py
@@ -5,5 +5,16 @@
 
 # some more stuff
 
-print("hell"
-"")
+print("hello")
+
+# test
+
+Grid = []
+Attempts = []
+player_one_sunk_ships = []
+player_two_sunk_ships = []
+player_one_ship_coordinates = []
+player_two_ship_coordinates = []
+player_one_bomb_attempts = []
+player_two_bomb_attempts = []
+
